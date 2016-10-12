8a7be85c5684752bad13bebc1528498a7a74c9c7
Luigy Leon <luigy@outlook.com>
2016-10-12 15:15:18 -0400

    FromJVSal aeson's Value instance.

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# Revision history for ghcjs-json
+
+## 0.1.0.0  -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ src/JavaScript/JSON/Types/FromJSVal.hs
@@ -0,0 +1,59 @@
+module JavaScript.JSON.Types.FromJSVal where
+
+import Control.Monad
+import Control.Monad.Trans.Maybe (MaybeT(..), runMaybeT)
+import qualified Data.Aeson as AE
+import qualified Data.HashMap.Strict as H
+import Data.Maybe
+import Data.Scientific (Scientific, scientific, fromFloatDigits)
+import qualified Data.Text as T
+import qualified Data.Text.Internal as T
+import qualified Data.Vector as V
+-- GHCJS
+import qualified Data.JSString.Text as JSS
+import GHCJS.Types
+import GHCJS.Foreign.Internal
+import GHCJS.Marshal.Internal
+import qualified JavaScript.Array as A
+import qualified JavaScript.Array.Internal  as AI
+import qualified JavaScript.Object as O
+import qualified JavaScript.Object.Internal as OI
+--
+import System.IO.Unsafe
+
+instance FromJSVal AE.Value where
+  fromJSVal r = case jsonTypeOf r of
+    JSONNull    -> return (Just AE.Null)
+    JSONInteger -> liftM (AE.Number . flip scientific 0 . (toInteger :: Int -> Integer))
+         <$> fromJSVal r
+    JSONFloat   -> liftM (AE.Number . (fromFloatDigits :: Double -> Scientific))
+         <$> fromJSVal r
+    JSONBool    -> liftM AE.Bool  <$> fromJSVal r
+    JSONString  -> liftM AE.String <$> fromJSVal r
+    JSONArray   -> liftM (AE.Array . V.fromList) <$> fromJSVal r
+    JSONObject  -> do
+        props <- OI.listProps (OI.Object r)
+        runMaybeT $ do
+            propVals <- forM props $ \p -> do
+                v <- MaybeT (fromJSVal =<< OI.getProp p (OI.Object r))
+                -- return (JSS.textFromJSString p, v)
+                return (T.Text p, v)
+            return (AE.Object (H.fromList propVals))
+  {-# INLINE fromJSVal #-}
+
+-- instance PFromJSVal AE.Value where
+--     pFromJSVal r = case jsonTypeOf r of
+--             JSONNull    -> (Just AE.Null)
+--             JSONInteger -> (AE.Number . flip scientific 0 . (toInteger :: Int -> Integer)) $ pFromJSVal r
+--             JSONFloat   -> (AE.Number . (fromFloatDigits :: Double -> Scientific)) $ pFromJSVal r
+--             JSONBool    -> AE.Bool $ pFromJSVal r
+--             JSONString  -> AE.String $ pFromJSVal r
+--             JSONArray   -> AE.Array . V.fromList $ pFromJSVal r
+--             JSONObject  -> fromJust $ unsafePerformIO $ do
+--                 props <- OI.listProps (OI.Object r)
+--                 runMaybeT $ do
+--                     propVals <- forM props $ \p -> do
+--                         v <- MaybeT (return . pFromJSVal =<< OI.getProp p (OI.Object r))
+--                         return (JSS.textFromJSString p, v)
+--                     return (AE.Object (H.fromList propVals))
+--     {-# INLINE pFromJSVal #-}
